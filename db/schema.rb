2732c66f1f2cc4b37197d57e274bb20fc1ecc409
Josh Cornelius <joshcornelius9@gmail.com>
2014-08-07 12:30:46 -0400

    added article editing and adding functionality

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140807151746) do
+ActiveRecord::Schema.define(version: 20140807161937) do
+
+  create_table "articles", force: true do |t|
+    t.string   "title"
+    t.string   "author"
+    t.string   "category"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "casein_admin_users", force: true do |t|
     t.string   "login",                           null: false
