ceba4b21857b9726f0afa5c8f7814be88eff0eac
Josh Cornelius <joshcornelius9@gmail.com>
2014-08-11 16:24:47 -0400

    further styling

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140811175634) do
+ActiveRecord::Schema.define(version: 20140811202414) do
 
   create_table "admins", force: true do |t|
     t.string   "username"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20140811175634) do
     t.string   "title"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "description"
   end
 
   create_table "polls", force: true do |t|
