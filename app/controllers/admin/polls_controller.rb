40fb06ed986da3b0dcb701a65b6599246eee1159
Josh Cornelius <joshcornelius9@gmail.com>
2014-08-11 13:24:22 -0400

    added polling admin functionality

--- app/controllers/admin/polls_controller.rb
+++ app/controllers/admin/polls_controller.rb
@@ -11,18 +11,36 @@ class Admin::PollsController < ApplicationController
 
 	def create
 		@poll = Poll.new(poll_params)
+		if @poll.save
+			redirect_to admin_polls_path
+		else
+			render :new
+		end
 	end
 
 	def edit
+		# @poll = Poll.find(params[:id])
 	end
 
 	def update
+		@poll = Poll.find(params[:id])
+		@poll.update_attributes(poll_params)
+		if @poll.save
+			redirect_to admin_polls_path
+		else
+			render :edit
+		end
+	end
+
+	def show
+		@poll = Poll.find(params[:id])
+		@options = @poll.options
 	end
 
 private
 
 	def poll_params
-		params.require(:poll).permit(:question)
+		params.require(:poll).permit(:question, options: [:id, :option_text, :_destroy])
 	end
 
 end
