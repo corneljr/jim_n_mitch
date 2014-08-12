855cd10d73b7b8a9c68bfdf9679981b6737da41c
Josh Cornelius <joshcornelius9@gmail.com>
2014-08-12 14:49:06 -0400

    updated production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,14 +20,14 @@ Rails.application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -80,4 +80,15 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      provider: 'AWS', 
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'], 
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+    }
+
+  config.fog_directory = 'sportsnstuff-prod'
+  config.fog_public = true
+  end
 end
