6992af6d21fd1eae923978d53ea4de5374ca8a44
Josh Cornelius <joshcornelius9@gmail.com>
2014-10-29 22:52:06 -0400

    poll

--- app/controllers/admin/votes_controller.rb
+++ app/controllers/admin/votes_controller.rb
@@ -4,6 +4,7 @@ class Admin::VotesController < ApplicationController
 		@vote = Vote.new
 		@vote.option_id = params[:choice]
 		@vote.save
+		@poll = Option.find(@vote.option_id).poll
 
 		if request.xhr?
 			render partial: 'poll'
